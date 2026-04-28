f60a5c883b8468ffbeae30cb9facfac599e86b17
Mohammad Kaif <mohammadkaif1110@gmail.com>
2026-04-28 23:33:09 +0530

    1st commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const app = express()
+app.get("/", (req,res)=>{
+    res.send("ledger Backend Server is running")
+})
+
 app.use(express.json())
 app.use(cookieParser())
 /*
